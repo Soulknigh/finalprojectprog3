be9f95ed0e9379991e5f81c4bc9fee67e0313ace
Your Name <you@example.com>
2019-05-23 19:14:55 +0400

    mnacele event@

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 var side = 20;
 var socket = io();
 
-var weatherclient= "Summer";
+var weatherclient = "Summer";
 
-socket.on("exanak", function(w){
+socket.on("exanak", function (w) {
     weatherclient = w;
 });
- //setup
- function setup() {
-    createCanvas(20 * side , 20 * side);
-    background('blue');  
- }
- 
- function drawWeather(w){
+//setup
+function setup() {
+    createCanvas(20 * side, 20 * side);
+    background('blue');
+}
+
+function drawWeather(w) {
     var p = document.getElementById('seasson');
     var weather = w;
     console.log(weather);
 
-    if(weather == "Summer"){
+    if (weather == "Summer") {
         p.innerText = "Summer";
     }
     else if (weather == "Winter") {
@@ -30,9 +30,9 @@ socket.on("exanak", function(w){
     else if (weather == "Spring") {
         p.innerText = "Spring";
     }
- }
- //nuyn draw functiony uxaki serveric ekac matrixi hashvin 
- function drawMatrix(matrix) {
+}
+//nuyn draw functiony uxaki serveric ekac matrixi hashvin 
+function drawMatrix(matrix) {
 
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
@@ -41,25 +41,46 @@ socket.on("exanak", function(w){
                 rect(x * side, y * side, side, side);
             }
             else if (matrix[y][x] == 1) {
-                fill("green");
-                rect(x * side, y * side, side, side);
+                if (weatherclient === "Summer") {
+                    fill("green");
+                } else if (weatherclient === "Summer") {
+                    fill("green");
+                }else if (weatherclient === "Spring") {
+                    fill("#9ACD32");
+                }else if (weatherclient === "Winter") {
+                    fill("#20B2AA");
+                }else if (weatherclient === "Autumn") {
+                    fill("#808000");
+                }
+               
             }
             else if (matrix[y][x] == 2) {
-                fill("yellow");
-                rect(x * side, y * side, side, side);
+                if (weatherclient === "Spring") {
+                    fill("yellow");
+                } else if (weatherclient !== "Spring") {
+                    fill("yellow");
+                }
+            
             }
             else if (matrix[y][x] == 3) {
-                fill("blue");
-                rect(x * side, y * side, side, side);
+                if (weatherclient === "Winter") {
+                    fill("blue");
+                } else if (weatherclient !== "Winter") {
+                    fill("blue");
+                }
+              
             }
             else if (matrix[y][x] == 4) {
-                fill("red");
-                rect(x * side, y * side, side, side);
+                if (weatherclient === "Autumn") {
+                    fill("red");
+                } else if (weatherclient !== "Autumn") {
+                    fill("red");
+                }
             }
             else if (matrix[y][x] == 5) {
                 fill("orange");
-                rect(x * side, y * side, side, side);
             }
+            rect(x * side, y * side,side,side);
         }
     }
 }
@@ -67,14 +88,3 @@ socket.on("exanak", function(w){
 //yndunuma serveric matrixy ev kanchuma drawMatrix
 socket.on("matrix", drawMatrix);
 socket.on("exanak", drawWeather);
-
-//function event kisata grac serverum el code petqa grvi sa vorpes hushum
-//  function mousePressed() {
-//     var x = Math.floor(mouseX / side);
-//     var y = Math.floor(mouseY / side);
-//     arr = [x, y];
-//     console.log(arr);
-//     socket.emit("Sxmvec", arr)
-
-// }
-
--- server.js
+++ server.js
@@ -30,9 +30,14 @@ xishnikArr = [];
 trexArr = [];
 triceratopsArr = [];
 
-Wheater = "Summer";
+var Wheater = "Summer";
 
-wheaterinit = 1;
+var wheaterinit = 1;
+Grassinit = 0
+GrassEaterinit = 0
+Predatorinit = 0
+TRexinit = 0
+Triceratopsinit = 0
 //stexcum en matrix generacnox function
 var w = 50;
 var h = 60;
@@ -45,10 +50,10 @@ function genMatrix(w, h) {
             var r = Math.floor(Math.random() * 100);
             if (r < 20) r = 0;
             else if (r < 30) r = 1;
-            else if (r < 55) r = 2;
-            else if (r < 75) r = 3;
-            else if (r < 85) r = 4;
-            else if (r < 100) r = 5;
+            else if (r < 45) r = 2;
+            else if (r < 65) r = 3;
+            else if (r < 75) r = 4;
+            else if (r < 90) r = 5;
             matrix[y][x] = r;
         }
     }
@@ -70,18 +75,23 @@ for (var y = 0; y < matrix.length; y++) {
 
         if (matrix[y][x] == 1) {
             grassArr.push(new Grass(x, y, 1));
+            Grassinit++;
         }
         else if (matrix[y][x] == 2) {
             grasseaterArr.push(new GrassEater(x, y, 2));
+            GrassEaterinit++;
         }
         else if (matrix[y][x] == 3) {
             xishnikArr.push(new Xishnik(x, y, 3));
+            Predatorinit++;
         }
         else if (matrix[y][x] == 4) {
             trexArr.push(new TRex(x, y, 4));
+            TRexinit++;
         }
         else if (matrix[y][x] == 5) {
             triceratopsArr.push(new Triceratops(x, y, 5));
+            Triceratopsinit++;
         }
     }
 }
@@ -111,7 +121,7 @@ function drawserver() {
 function draw_wheater() {
     wheaterinit++;
     if(wheaterinit == 5){
-        wheaterinit =1;
+        wheaterinit = 1;
     }
     if(wheaterinit == 4){
         Wheater ="Autumn";
@@ -128,6 +138,12 @@ function draw_wheater() {
 
     io.sockets.emit("exanak", Wheater);
 }
-
+var obj = {"info" : []};
+function main(){
+    var file = "Statistics.json";
+    obj.info.push({"Cnvac xoteri kanak@": Grassinit, "Cnvac xotakerneri kanak@": GrassEaterinit, "Cnvac predatorneri kanak@": Predatorinit, "Cnvac TRexneri kanak@": TRexinit, "Cnvac Triceratopsneri kanak@": Triceratopsinit})
+    fs.writeFileSync(file,JSON.stringify(obj,null,5));
+}
 setInterval(drawserver, 2000);
-setInterval(draw_wheater, 4000);
+setInterval(draw_wheater, 6000);
+setInterval(main, 3000);
